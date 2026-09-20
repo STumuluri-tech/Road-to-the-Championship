c9409ca84e1cf46b8ddf310b4108e66097bbf9be
Satyateja Tumuluri <104220416+STumuluri-tech@users.noreply.github.com>
2026-09-20 00:30:36 +0000

    added some startong code

--- Main.java
+++ Main.java
@@ -1,1 +1,41 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        StackPane intro_stuff = new StackPane();
+
+        Image introScreen = new Image(getClass().getResourceAsStream("/IntroScreen.png"));
+
+        BackgroundImage backgroundImage = new BackgroundImage(
+            introScreen,
+            BackgroundRepeat.NO_REPEAT,  
+            BackgroundRepeat.NO_REPEAT,  
+            BackgroundPosition.CENTER,   
+            new BackgroundSize(
+                BackgroundSize.AUTO,     
+                BackgroundSize.AUTO,     
+                false,                   
+                false,                   
+                false,                   
+                true                     
+            )
+        );
+
+        intro_stuff.setBackground(new Background(backgroundImage));
+
+        primaryStage.setScene(new Scene(intro_stuff, 768,384));
+        primaryStage.show();
+
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
